1b359dd17af45be5f9286bb875cc2b49179ac575
Amosov Alex <alex_amos@mail.ru>
2023-03-04 12:43:07 +0400

    3_1_3

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserByUsername(String username);
+
+    @Query("select u from User u join fetch u.roles where u.username =:username")
+    User getUserWithRole(@Param("username") String username);
 }
--- demo/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,10 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Role;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,20 +9,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService, UserDetailsService {
-    @PersistenceContext
-    private final EntityManager entityManager;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(EntityManager entityManager, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.entityManager = entityManager;
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -75,7 +67,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = entityManager.createQuery("select u from User u join fetch u.roles ", User.class).getResultList().get(0);
+        User user = userRepository.getUserWithRole(username);
 
         if (user == null) {
             throw new UsernameNotFoundException(String.format("User with %s not found", username));
